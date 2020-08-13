26f722061382a9322c944cb40aed52496821facb
cameronkenny101 <55995394+cameronkenny101@users.noreply.github.com>
2020-08-13 14:03:51 +0100

    Used php for server side scripting

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'DublinSushi@csserver.ucd.ie';
+    $email_subject = "New Form Submit";
+    $email_body = "User Name : $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+
+    $to = "cameron.kenny1@ucdconnect.ie"
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location: index.html");
+?>
